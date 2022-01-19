e9ba76d91c14a38198b670a7afd3440f2572448e
meicale <meicale@163.com>
2022-01-19 15:10:30 +0800

    add some config for git with fugitive

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -9,6 +9,8 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.g.surround_prefix = "s"
+-- vim.g.surround_load_autogroups = true
 
 -- Modes
 --   normal_mode = "n",
